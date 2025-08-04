97fa9930129b74c7c9d2791e4fdbf9c3be59af96
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-08-04 12:56:35 +0100

    Bump version v0.3.0 -> v0.3.0.post1

--- sphinx_packaging/__init__.py
+++ sphinx_packaging/__init__.py
@@ -43,7 +43,7 @@ __all__ = ["setup"]
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2021 Dominic Davis-Foster"
 __license__: str = "BSD License"
-__version__: str = "0.3.0"
+__version__: str = "0.3.0.post1"
 __email__: str = "dominic@davis-foster.co.uk"
 
 
